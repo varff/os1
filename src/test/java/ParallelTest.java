d949895029d8e4129c76ffcc624239a2ea596da9
varff <62174550+varff@users.noreply.github.com>
2021-02-18 23:49:56 +0300

    First add

---
+++ src/test/java/ParallelTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+
+public class ParallelTest {
+
+	List<List<Integer>> matrix = (Arrays.asList(
+			Arrays.asList(1,2,3,-3,2,0),//3
+			Arrays.asList(1,2,4,-3,2,8),//8
+			Arrays.asList(1,0,0,0,-2,0),//1
+			Arrays.asList(1,0,11,0,6,0)//11
+	));
+
+	@Test
+	public void sumParallel() throws InterruptedException {
+
+		int result = Parallel.sumParallel(matrix,3);
+		assertEquals(result, 23);
+
+	}
+
+	@Test
+	public void sumParallelStream() throws InterruptedException, ExecutionException {
+		int result = Parallel.sumParallelStream(matrix,5);
+		assertEquals(result, 23);
+	}
+}
